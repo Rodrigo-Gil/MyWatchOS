21d800faa98a28bd6027971ef1c2e62b62e09ec8
Rodrigo Gil <gil00013@algonquinlive.com>
2022-02-23 11:55:07 -0500

    initial watchOS project created .

--- MyWatchOS WatchKit Extension/ContentView.swift
+++ MyWatchOS WatchKit Extension/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack(alignment: .center, spacing: 8){
+            Text("Our Counter will be here")
+                .padding()
+        }
     }
 }
 
